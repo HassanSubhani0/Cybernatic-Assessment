08290837130b4e46db21103ab7507c37efeb238f
Ahsan Subhani <hassansubhani808@gmail.com>
2024-06-06 00:13:54 +0500

    Automated APi's

---
+++ cypress/default/createAndGetStations.cy.js
@@ -0,0 +1,45 @@
+//import test data for the POST request
+import { createStations } from "../fixtures/createStations";
+//instantiates the request body - the result is an array of objects
+let stationData = createStations(),
+  stationId;
+
+stationData.stations.forEach((stationData) => {
+  it(`${stationData.TestCase}`, () => {
+    let { TestCase, ...requestBody } = stationData;
+
+    //Post the api request
+    cy.request({
+      method: "POST",
+      url: Cypress.config("baseUrl"),
+      qs: { appid: Cypress.env("appid") },
+      body: requestBody,
+      headers: { "Content-Type": "application/json" },
+    }).then((response) => {
+      expect(response.status).to.eq(201);
+      expect(response.body.ID).to.match(/^[a-z0-9]+$/i);
+      expect(response.body.rank).to.be.a("number");
+      expect(response.body.source_type).to.be.a("number");
+
+      let {
+        ID,
+        updated_at,
+        created_at,
+        user_id,
+        source_type,
+        rank,
+        ...responseBody
+      } = response.body;
+      expect(responseBody, "The API Response : ").to.deep.equal(requestBody);
+      stationId = response.body.ID;
+      cy.getStation(stationId).then((response) => {
+        expect(response.status).to.eq(200);
+        expect(response.body.id).to.eq(stationId);
+        let { id, created_at, updated_at, rank, ...responseBody } =
+          response.body;
+        expect(requestBody, "The API Response : ").to.deep.equal(responseBody);
+        cy.log(JSON.stringify(response.body));
+      });
+    });
+  });
+});
